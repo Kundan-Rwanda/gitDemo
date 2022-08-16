54d9c5943ae1b4a5adda5e034d5a103de0253344
Kundan-Rwanda <kundan.kumar011@gmail.com>
2022-08-16 17:18:28 +0200

    Updated sum.java and added sub.java

--- sub.java
+++ sub.java
@@ -1,9 +1,9 @@
-public class sum
+public class sub
 {
     public static void main(String args[])
     {
         int x = 10, y=20;
     
-        System.out.println("Sum : "+(x+y));
+        System.out.println(x-y);
     }
 }
